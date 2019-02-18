448cefcdbffa2ec2bdfa2f07b8926cd56e6d3b2f
RainHearer <1217238681@qq.com>
2019-02-18 18:44:33 +0800

    int-version

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//开启调试模式
+    define('APP_DEBUG', 1);
+    //定义项目名称
+    define('APP_NAME', 'index');
+    //定义项目路径
+    define('APP_PATH', './index/');
+    //加载框架入文件
+    require './ThinkPHP/ThinkPHP.php';
